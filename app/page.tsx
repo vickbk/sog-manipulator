244b25804fd01abdf302bb9e612b608818362174
vickbk <vickbake8@gmail.com>
2025-11-22 14:00:53 +0200

    Initing SOG Text manipulator project

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Metadata } from "next";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SOG | Song of God - Text Manipulator",
+};
+
+export default function Home() {
+  return <></>;
+}
