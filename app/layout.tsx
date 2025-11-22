f31d149d1e3a9c3bdd766bf4fd22f0fec2e77ce7
vickbk <vickbake8@gmail.com>
2025-11-22 14:09:00 +0200

    Adding the main header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import MainHeader from "../components/common/MainHeader";
 import "../styles/global.css";
 
 export default function RootLayout({
@@ -7,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainHeader />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/common/MainHeader.tsx
@@ -0,0 +1,7 @@
+export default function MainHeader() {
+  return (
+    <header>
+      <h1>Welcome to the SOG Text manipulator app</h1>
+    </header>
+  );
+}
